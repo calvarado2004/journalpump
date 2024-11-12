c03ec20b92f9171dd97ad85e37e83ce46d4edf61
Carlos Alvarado Martinez <45523697+calvarado2004@users.noreply.github.com>
2024-11-12 16:31:22 -0500

    Update elasticsearch_opensearch_sender.py
    Set include_type_name to false

--- journalpump/senders/elasticsearch_opensearch_sender.py
+++ journalpump/senders/elasticsearch_opensearch_sender.py
@@ -312,7 +312,7 @@ class ElasticsearchSender(_EsOsLogSenderBase):
             raise ValueError("Version has not been set")
         index_url = super()._index_url(index_name)
         if self._version.major <= self._VERSION_WITH_MAPPING_TYPE_SUPPORT:
-            index_url = f"{index_url}?include_type_name=true"
+            index_url = f"{index_url}?include_type_name=false"
         return index_url
 
     def _message_header(self, index_name: str) -> Dict[str, Any]:
